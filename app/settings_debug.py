f8bb507af93ac7d8360f3f4d2ad1aafa2c720168
Mick <mmailm@mail.ru>
2014-03-12 14:53:28 +0900

    добавил google-analytics, страницы: 404, 505 пофиксил небольшие баги в шаблонах, добавил robots.txt и sitemap.xml

---
+++ app/settings_debug.py
@@ -0,0 +1,6 @@
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+# Hosts/domain names that are valid for this site; required if DEBUG is False
+# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = ['*', 'localhost', 'www.irgid.ru', 'irgid.ru']
--- app/urls.py
+++ app/urls.py
@@ -5,13 +5,19 @@ from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
+from django.views.generic import TemplateView
+
 admin.autodiscover()
 
 urlpatterns = i18n_patterns('',
+                            (r'^robots\.txt$', TemplateView.as_view(
+                                template_name='robots.txt', content_type='text/plain')),
+                            (r'^sitemap\.xml$', TemplateView.as_view(
+                                template_name='sitemap.xml', content_type='text/plain')),
                             url(r'^gallery/', include('image_gallery.urls')),
                             url(r'^admin/', include(admin.site.urls)),
                             url(r'^', include('cms.urls')),
-)
+                            )
 
 if settings.DEBUG:
     urlpatterns = patterns('',
